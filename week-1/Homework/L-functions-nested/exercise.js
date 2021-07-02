f1d6dd4feaaa7bcbbf2a695fc27d6941403602da
Omar <omared250@gmail.com>
2021-07-02 23:03:55 +0200

    exercise 1 L-Homework

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,5 +1,19 @@
+function mentorName(name) {
+    return name.toUpperCase();
+}
+
+function greetingAMentor(name, greeting) {
+    return greeting.toUpperCase() + mentorName(name);  
+}
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+console.log(greetingAMentor(mentor1, "hello, "))
+console.log(greetingAMentor(mentor2, "hello, "))
+console.log(greetingAMentor(mentor3, "hello, "))
+console.log(greetingAMentor(mentor4, "hello, "))
+console.log(greetingAMentor(mentor5, "hello, "))
