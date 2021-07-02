49b1148199e371bae427a669ff60d217906a4dd8
Omar <omared250@gmail.com>
2021-07-02 13:33:27 +0200

    exercise F

---
+++ week-1/InClass/exercise-F.js
@@ -0,0 +1,10 @@
+const numbersOfStudents = 18;
+const numbersOfMentors = 5;
+
+const totalNumbersOfPersons = numbersOfStudents + numbersOfMentors;
+
+console.log("The numbers of Students are = " + numbersOfStudents);
+console.log("The numbers of Mentors are = " + numbersOfMentors);
+console.log("The total between the students and mentors are = " + totalNumbersOfPersons);
+
+console.log(typeof(totalNumbersOfPersons));
