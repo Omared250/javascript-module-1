7bbfc6a845680f78c0fcfc3d852b161c406a1d46
Omar <omared250@gmail.com>
2021-07-02 14:23:37 +0200

    exercise G

---
+++ week-1/InClass/exercise-G.js
@@ -0,0 +1,14 @@
+const numbersOfStudents = 18;
+const numbersOfMentors = 5;
+
+const totalOfPersons = numbersOfMentors + numbersOfStudents;
+
+var percentageMentors = (numbersOfMentors / totalOfPersons)*100;
+var percentageStudents = (numbersOfStudents / totalOfPersons)*100;
+
+var intpercentageMentors = Math.round(percentageMentors);
+var intpercentageStudents = Math.round(percentageStudents);
+
+console.log(intpercentageMentors + "% of the class are Mentors");
+console.log(intpercentageStudents + "% of the class are Students");
+
