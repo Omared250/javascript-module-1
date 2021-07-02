fb92de54714284b0dfc3674ad9f9c830976f3b97
Omar <omared250@gmail.com>
2021-07-02 17:34:41 +0200

    exercise 4 homework

--- week-1/Homework/J-functions/exercise2.js
+++ week-1/Homework/J-functions/exercise2.js
@@ -1,7 +1,10 @@
 function triple(number) {
-  // complete function here
+  // complete function 
+  return number = number * 3;
 }
 
 var result = triple(12);
+var result4 = triple(150);
 
 console.log(result);
+console.log(result4);
