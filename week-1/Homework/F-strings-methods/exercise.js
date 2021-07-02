c03f626307e0803a7548a93348782ea4d52fe389
Omar <omared250@gmail.com>
2021-07-02 17:13:34 +0200

    exercise 1 homework

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
 
+const name = "Omar";
+const lengthName = name.length;
+
+const message = "My name is Omar and my name is " + lengthName + " characters long!"
+
 console.log(message);
