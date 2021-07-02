882b2e2196b0e20b7a63ab12651edaef4a018370
Omar <omared250@gmail.com>
2021-07-02 17:22:13 +0200

    exercise 2 homework

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,7 @@
-const name = " Daniel  ";
+const name1 = " Omar  ";
+const withoutSpace = name1.trim();
+const lengthName1 = withoutSpace.length;
 
-console.log(message);
+const message1 = "My name is Omar and my name is " + lengthName1 + " characters long!"
+
+console.log(message1);
