62c12061a01d0168919332377c742ed82973f898
Omar <omared250@gmail.com>
2021-07-02 17:55:39 +0200

    exercise 4 K-Homework

--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
+function addingTwoNumbers(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
 
 // Call the function and assign to a variable `sum`
-
+var sum = addingTwoNumbers(13, 124);
 console.log(sum);
