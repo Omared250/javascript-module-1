ecceca7111596a41357fee1012074b70df30b489
Omar <omared250@gmail.com>
2021-07-02 17:32:04 +0200

    exercise 3 homework

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,12 @@
 function halve(number) {
   // complete the function here
+  return number / 2;
 }
 
 var result = halve(12);
+var result1 = halve(17);
+var result2 = halve(150);
 
 console.log(result);
+console.log(result1);
+console.log(result2);
