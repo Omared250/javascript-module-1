0654627ee4b7f3424f255026f59bcde3ecdd4b02
Omar <omared250@gmail.com>
2021-07-03 13:55:04 +0200

    fixing function-output errors

--- week-1/Extra/3-function-output.js
+++ week-1/Extra/3-function-output.js
@@ -1,16 +1,18 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getNumber() {
   return Math.random() * 10;
-}
+} // Returns a random number between 0 (included) and 1 (excluded)
 
 // Add comments to explain what this function does. You're meant to use Google!
 function s(w1, w2) {
   return w1.concat(w2);
-}
+} /* This function is used to join two or more arrays. This method does not change the existing arrays,
+      but returns a new array*/
 
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord + " " + secondWord + " " + thirdWord;
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
