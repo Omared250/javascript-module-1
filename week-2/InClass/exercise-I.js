bc10e52eb872a94ee5fd71303ad8af69f64537f1
Omar <omared250@gmail.com>
2021-07-08 13:40:31 +0200

    week 2 exercise-I

---
+++ week-2/InClass/exercise-I.js
@@ -0,0 +1,13 @@
+/*
+> const fruits = ['banana', 'apple', 'strawberry', 'kiwi', 'fig', 'orange'];
+undefined
+> console.log(fruits[2]);
+strawberry
+> console.log(fruits[3]);
+kiwi
+> console.log(fruits[5]);
+orange
+> console.log(fruits[0]);
+banana
+
+ */
