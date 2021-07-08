5268f5ca203277bf3903369139a25fd1649d6683
Omar <omared250@gmail.com>
2021-07-08 13:11:04 +0200

    week 2 exercise-F

--- week-2/InClass/exercise-F.js
+++ week-2/InClass/exercise-F.js
@@ -1,7 +1,10 @@
-function validUser(userName) {
-    if (userName.length > 5 && userName.length < 10) {
+function validUser(userName, userType) {
+    if (((userName.length > 5 && userName.length < 10) && (userName.charAt(0) === userName.charAt(0).toUpperCase())) && (userType === "admin" || userType === "manager")) {
         return "Username Valid."
+    } else {
+        return "Username Invalid."
     }
 }
 
-console.log(validUser("Carlos"))
+console.log(validUser("Carlos", "admin"))
+console.log(validUser("omar", "admin"))
