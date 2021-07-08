ddda2cba3a2bd9d7e392456c03b467f5b6ebd133
Omar <omared250@gmail.com>
2021-07-08 12:02:08 +0200

    week 2 exercise-A

---
+++ week-2/InClass/exercise-A.js
@@ -0,0 +1,14 @@
+/*
+> 1+2
+3
+> "hello"
+'hello'
+> let favoriteColor = "purple";
+undefined
+> favoriteColor
+'purple'
+> console.log(favoriteColor);
+purple
+
+These are the answers of the exercise A working in the terminal of NODE
+*/
