8cc8dbc42f06dab00f9543b26c506ca807555f24
Omar <omared250@gmail.com>
2021-07-08 13:24:40 +0200

    week 2 exercise-G

--- week-2/InClass/exercise-G.js
+++ week-2/InClass/exercise-G.js
@@ -1,4 +1,11 @@
 var apolloCountdownMessage = "all engine running... LIFT-OFF!";
 var countdown = 8;
 
+while (countdown >= 0) {
+    console.log("The count is: " + countdown);
+    countdown -= 1;
+}
+
 console.log(apolloCountdownMessage);
+
+
