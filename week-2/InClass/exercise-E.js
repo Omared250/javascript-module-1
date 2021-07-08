34fd2f71ab255daf73fd77f01ba0c7e022d6933c
Omar <omared250@gmail.com>
2021-07-08 12:13:29 +0200

    week 2 exercise-E

---
+++ week-2/InClass/exercise-E.js
@@ -0,0 +1,18 @@
+/*
+> let num = 10
+undefined
+> num > 5 && num < 5
+false
+> num < 10 || num === 10
+true
+> false || true
+true
+> !true
+false
+> let greaterThan5 = num > 10
+undefined
+> !greaterThan5
+true
+> !(num === 10)
+false
+*/
