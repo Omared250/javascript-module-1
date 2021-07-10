7f3e5569184656935e8c326092db7e9a39c1eb88
Omar <omared250@gmail.com>
2021-07-10 13:21:33 +0200

    Inclass exercise H

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -5,3 +5,10 @@ function exponential(number) {
 function isEven(number) {
   return number % 2 === 0;
 }
+
+for (number = 5; number < 20; number ++) {
+  if(isEven(number)) {
+    let result = exponential(number);
+    console.log("The exponential of " + number + " is " + result);
+  }
+} 
