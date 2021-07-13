cba4534ce89613f810661e01aeee9842bb8fbfbf
Omar <omared250@gmail.com>
2021-07-13 19:03:30 +0200

    exercise B week 3

--- week-3/InClass/exercise-B.js
+++ week-3/InClass/exercise-B.js
@@ -1,1 +1,19 @@
 
+let arrayListOfTheClass = ["Omar", "Bianca", "Ananda", "Rana", "Diego", "Fran", "Elmira", "Suman"];
+let arrayAnotherClass = ["Maxwell", "William", "Luiza", "Said", "Amritpal", "Laura"];
+
+let combinedArrayClasses = arrayListOfTheClass.concat(arrayAnotherClass);
+
+
+function returningString (name, arrayClass) {
+    if (arrayClass.includes(name)) {
+        return name + " is at the class with " + arrayClass;
+    } 
+    else {
+        return name + " is not at the class with " + arrayClass;
+    }
+}
+
+console.log(combinedArrayClasses.sort());
+console.log(returningString("Omar", arrayListOfTheClass));
+console.log(returningString("Omar", arrayAnotherClass));
