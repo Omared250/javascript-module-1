9e63fbaa4dcbcde4fab8ef9c5d972ccdb3fb47d6
Omar <omared250@gmail.com>
2021-07-14 13:00:04 +0200

    exercise F week 3

--- week-3/InClass/exercise-F.js
+++ week-3/InClass/exercise-F.js
@@ -1,17 +1,14 @@
 years = [ 1964, 2008, 1999, 2005, 1978, 1985, 1919 ];
 
-function availabelToDrive(years) {
-    years.forEach((year) => {
-        let age = 2021 - year;
-        if (age > 17) {
-            return "You can drive!"
-        } 
-        else {
-            return "You can't drive!"
-        }
-    });
-
-    return age;
+function availableToDrive(year) {
+    let age = 2021-year;
+    if(age >= 17) {
+        return "Born in " + year + " can drive";
+    } else {
+        return "Born in " + year + " can drive in " + (17-age) + " years";      
+    }
 }
 
-console.log(availabelToDrive(years));
+const canDrive = years.map(availableToDrive);
+
+console.log(canDrive);
