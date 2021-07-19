afff879edb1042c320223e2f14dee7e5356a6bf4
Omar <omared250@gmail.com>
2021-07-19 12:38:06 +0200

    fixing exercise G InClass

--- week-3/InClass/exercise-G.js
+++ week-3/InClass/exercise-G.js
@@ -1,14 +1,13 @@
 years = [ 1964, 2008, 1999, 2005, 1978, 1985, 1919 ];
 
 function yearsAvailabelToDrive (years) {
-    years.map((year) => {
+    years.filter((year) => {
         if (year <= 2002) {
-            return year;
+            return console.log("These are the birth years of people who can drive: " + year);
         }
-    }).filter((canDrive) => console.log("These are the birth years of people who can drive: " + canDrive));
+    })
 }
 
 const canDrive = yearsAvailabelToDrive(years);
-
 console.log(canDrive);
 
